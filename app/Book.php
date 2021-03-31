2fcb56ce07688939e8a261f693ca9ad2f8013353
Jordy Tshibangu <jordytshibss@gmail.com>
2021-03-31 14:34:48 +0200

    features : adding relationship between entities

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    public function user(){
+        
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(){
+
+        return $this->hasOne(Category::class);
+        
+    }
 }
