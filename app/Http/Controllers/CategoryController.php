5e35a5e1d2dc27f96ef834357719c6cc7698813a
Jordy Tshibangu <jordytshibss@gmail.com>
2021-04-12 18:28:28 +0200

    features : delete and update functionality in Category controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,33 +10,62 @@ class CategoryController extends Controller
     
     public function index()
     {
-        // $categories = Category::find();
+        $categories = Category::all();
 
-        return "hello world bonjour";
+        return response()->json([
+            'Message' => 'Categories successfully retrieved',
+            'Category' => $categories,
+        ], 200); 
     }
 
     public function store(Request $request)
     {
-        
+        $request->validate([
+            'name' => 'required'
+        ]);
+
+        $category = Category::create([
+            'name' => $request->name
+        ]);
+
+        return response()->json([
+            'Message' => 'Category successfully Created',
+            'Category' => $category,
+        ],202);
     }
 
-    public function show(Category $category)
+    public function show(Category $id)
     {
-        $id = $category->_id;
-
         $category = Category::findOrFail($id);
 
-        return $category;
-        
+        return response()->json([
+            'Message' => 'Category successfully retrieved',
+            'Category' => $category,
+        ],200);  
     }
 
     public function update(Request $request, Category $category)
     {
+        $category_id =  $category->id;
+        $category = Category::where('id', $category_id)->update(['name' => $request->name]);
+        $updated_category = Category::findOrFail($category_id);
+
+        return response()->json([
+            'Message' => 'Category successfully Updated',
+            'Category' => $updated_category,
+        ],204);
         
     }
     
     public function destroy(Category $category)
     {
-        
+        $category_id =  $category->id;
+        $category = Category::findOrFail($category_id);
+        $category->delete();
+
+        return response()->json([
+            'Message' => 'Category successfully deleted',
+            'Category' => $category,
+        ],200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,8 @@ use App\Http\Controllers\CustomerController;
 Route::get('/category', [CategoryController::class, 'index']);
 Route::get('/category/{id}', [CategoryController::class, 'show']);
 Route::post('/category', [CategoryController::class, 'store']);
-Route::put('/category/{id}', [CategoryController::class, 'update']);
-Route::delete('/category/{id}', [CategoryController::class, 'destroy']);
+Route::put('/category/{category}', [CategoryController::class, 'update']);
+Route::delete('/category/{category}', [CategoryController::class, 'destroy']);
 
 # Book routes
 
